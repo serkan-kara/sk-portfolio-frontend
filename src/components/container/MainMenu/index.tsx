e213ae3c7d4d0df94a3247b2841095f7a0f4031e
Serkan Kara <b.serkankara@gmail.com>
2023-08-23 16:46:13 +0300

    menu update

--- src/components/container/MainMenu/index.tsx
+++ src/components/container/MainMenu/index.tsx
@@ -1,5 +1,11 @@
 const MainMenu = () => {
-	return <>Menu</>;
+	return <>
+		<ul className="flex space-x-4">
+			<li>About Me</li>
+			<li>Projects</li>
+			<li>Contact</li>
+		</ul>
+	</>;
 };
 
 export { MainMenu };
