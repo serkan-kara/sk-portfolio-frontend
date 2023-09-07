48bfa0a8ee897150f7a0e47b8c57512c9ca7702e
Serkan Kara <b.serkankara@gmail.com>
2023-09-07 16:56:57 +0300

    cosmetic updates

--- src/components/container/Skillset/index.tsx
+++ src/components/container/Skillset/index.tsx
@@ -9,9 +9,9 @@ import { Skill } from "../Skill";
 
 const Skillset = () => {
 	return (
-		<div className='flex flex-col py-10 mt-10'>
-			<Text type='h1' className='text-4xl text-folly font-bold'>
-				Some of my techstack
+		<div className='flex flex-col py-10 mt-10 space-y-6'>
+			<Text type='h1' className='text-2xl text-folly font-bold border-white border-b border-opacity-10 pb-4'>
+				Tech Stack
 			</Text>
 			<div className='grid grid-flow-row gap-4 md:grid-cols-3'>
 				<Skill name='ReactJS' skillIcon={ReactJSIconSVG} experience={5} rating={5} />
